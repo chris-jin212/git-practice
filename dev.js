86d6229973dfa6fdf735ab533cfc036a98c2c687
webdev0725 <webdev0725@hotmail.com>
2020-12-08 21:43:34 +0800

    feature/12th added

--- dev.js
+++ dev.js
@@ -21,6 +21,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webpack tutorial</title>
+</head>
+<body>
+<h1>Hello webpack!</h1>
+<h2>Change benchmark profile</h2>
+</body>
+</html>
     <meta charset="UTF-8">
     <title>Webpack tutorial</title>
 </head>
