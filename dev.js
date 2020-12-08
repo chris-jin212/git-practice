14a5efdef17ed1430882a439b419e2e3ac859abe
webdev0725 <webdev0725@hotmail.com>
2020-12-08 21:43:16 +0800

    feature/11th added

--- dev.js
+++ dev.js
@@ -18,5 +18,16 @@
 <h1>Hello webpack!</h1>
 <h2>Change benchmark profile</h2>
 </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webpack tutorial</title>
+</head>
+<body>
+<h1>Hello webpack!</h1>
+<h2>Change benchmark profile</h2>
+</body>
+</html>
 </html>
 </html>
